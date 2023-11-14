0bf6eb28fae5dbf346f438024ce5ef33a06a93df
tuzi555 <jakub.tuzar@gmail.com>
2023-11-14 10:21:46 +0100

    Shadcn theming

--- app/page.tsx
+++ app/page.tsx
@@ -10,5 +10,9 @@ export default async function Home() {
       ORDER BY "createdAt" DESC LIMIT 1
   `)
   )[0] as SelectPost;
-  return <div className="flex justify-center"><h1>Hello from app router {post.content}</h1></div>;
+  return (
+    <div className="flex justify-center">
+      <h1>Hello from app router {post.content}</h1>
+    </div>
+  );
 }
